36a11db9b3509c92ef62d286c550d2550eca5ac0
AndreasZachariae <Andreas.Zachariae@gmail.com>
2025-05-30 16:14:38 +0200

    Move data folder and add dockerignore

--- start_docker.sh
+++ start_docker.sh
@@ -31,6 +31,7 @@ docker run \
     -v $XAUTHORITY:$XAUTHORITY:rw \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $PWD/src:/home/docker/ros_ws/src \
+    -v $PWD/data:/home/docker/ros_ws/data \
     -v /dev:/dev  \
     ${CONTAINER_NAME}:${CONTAINER_TAG} \
     ${COMMAND}
