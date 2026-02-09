972ac64968de9baf8f5333e529afa78c5732ac45
AndreasZachariae <Andreas.Zachariae@gmail.com>
2026-02-09 11:27:39 +0100

    Reduce topics to capture. Move merging und trimming to later stage.

--- src/main.py
+++ src/main.py
@@ -22,21 +22,21 @@ def main():
     processor_gait.process_all_bags(folder=bag_folder_gait)
     print("[MAIN] Done processing all gait bags.")
 
-    merger = CSVMerger(
-        base_csv="data/KATE_AA_dataset.csv",
-        gait_csv="data/KATE_AA_dataset_gait.csv",
-        output_csv="data/KATE_AA_merged.csv",
-        tolerance=0.05
-    )
-    merger.merge()
+    # merger = CSVMerger(
+    #     base_csv="data/KATE_AA_dataset.csv",
+    #     gait_csv="data/KATE_AA_dataset_gait.csv",
+    #     output_csv="data/KATE_AA_merged.csv",
+    #     tolerance=0.05
+    # )
+    # merger.merge()
 
-    trimmer = CsvTrimmer(
-        csv_path=output_csv,
-        force_col="force_input_raw_x",
-        ref_cols=("path_index_front", "path_index_left", "path_index_right"),
-        out_folder="data"
-    )
-    trimmer.process("KATE_AA_trimmed.csv")
+    # trimmer = CsvTrimmer(
+    #     csv_path=output_csv,
+    #     force_col="force_input_raw_x",
+    #     ref_cols=("path_index_front", "path_index_left", "path_index_right"),
+    #     out_folder="data"
+    # )
+    # trimmer.process("KATE_AA_trimmed.csv")
 
 
 if __name__ == "__main__":
